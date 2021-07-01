9215cc2977175467ca94f22e7b0b978ce7e236f3
Administrator <Administrator@kohasaeng.local>
2021-07-01 14:53:42 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//preloader
+$(window).on('load', function () {
+	$('.status').fadeOut();
+	$('#preloader').delay(500).fadeOut();
+});
+
+var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	typeSpeed: 100,
+	loop: true,
+	loopCount: Infinity,
+});
+
+$('.slider').slick({
+	speed: 1000,
+	arrows: false,
+	dots: false,
+	autoplay: true,
+	fade: true,
+	cssEase: 'linear',
+});
